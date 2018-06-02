29ced71ec03898d4d5a875abac1276aa5441b711
Jerome <2234064569@qq.com>
2018-06-02 23:27:56 +0800

    rename all file

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@ def rename():
     import os
 
     for file in os.listdir('.'):
-        if file[1].isdigit() and file[-2:] != 'py':
+        if file[1].isdigit() and file[-2:] == 'py':
             # os.rename()
-            print('文件夹', file)
-            os.rename(os.path.join(file, '__init__.py'), file + '.py')
+            print('_0' + file[1:])
+            os.rename(file, '_0' + file[1:])
 
 
 if __name__ == '__main__':
